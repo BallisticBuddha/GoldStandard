85c4b9a27e15215b6925f8e5159ab47608a35542
Ballistic Buddha <ballisticbuddha@gmail.com>
2011-07-22 12:19:38 -0400

    first steps to custom parent items for relative items

--- createtable.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS `gslog` (
-`pkGSlog` int(10) unsigned NOT NULL AUTO_INCREMENT,
-`time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-`amount` int(10) unsigned NOT NULL DEFAULT '1',
-`user` varchar(45) DEFAULT NULL,
-PRIMARY KEY (`pkGSlog`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1
